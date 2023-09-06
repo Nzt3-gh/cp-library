57e44fa3247f2c42300cc10548c251771dd3a01d
Nzt3 <nzt3pro@gmail.com>
2023-09-06 19:07:08 +0900

    add ll_floor

---
+++ math/ll_floor.hpp
@@ -0,0 +1,14 @@
+
+long long llceil(long long a,long long b){
+  if(a%b==0){return a/b;}
+ 
+  if(a>=0){return (a/b)+1;}
+  else{return -((-a)/b);}
+}
+
+long long llfloor(long long a,long long b){
+  if(a%b==0){return a/b;}
+ 
+  if(a>=0){return (a/b);}
+  else{return -((-a)/b)-1;}
+}
