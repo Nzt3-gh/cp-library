0d7ce9224451f1d94977a80b2935281298b62e5f
Nzt3 <nzt3pro@gmail.com>
2023-09-06 19:09:11 +0900

    fix namespace

--- math/ll_floor.hpp
+++ math/ll_floor.hpp
@@ -1,14 +1,15 @@
+namespace Lib{
+  long long llceil(long long a,long long b){
+    if(a%b==0){return a/b;}
+  
+    if(a>=0){return (a/b)+1;}
+    else{return -((-a)/b);}
+  }
 
-long long llceil(long long a,long long b){
-  if(a%b==0){return a/b;}
- 
-  if(a>=0){return (a/b)+1;}
-  else{return -((-a)/b);}
-}
-
-long long llfloor(long long a,long long b){
-  if(a%b==0){return a/b;}
- 
-  if(a>=0){return (a/b);}
-  else{return -((-a)/b)-1;}
+  long long llfloor(long long a,long long b){
+    if(a%b==0){return a/b;}
+  
+    if(a>=0){return (a/b);}
+    else{return -((-a)/b)-1;}
+  }
 }
