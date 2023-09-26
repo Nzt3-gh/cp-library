8ea44699518ba76d4683f3421eac14c5499d415f
Nzt3 <nzt3pro@gmail.com>
2023-09-26 15:14:51 +0900

    fix namespace

--- string/z_algorithm.hpp
+++ string/z_algorithm.hpp
@@ -4,23 +4,24 @@
 verified https://judge.yosupo.jp/submission/161223
 vector の longest common prefix を求める
 */
-
-template<class T>
-vector<int> z_algorithm(const vector<T>& A){
-  int n=A.size();
-  vector<int> z(n);
-  int i=1,j=0;
-  while(i<n){
-    while(i+j<n&&A[j]==A[i+j])j+=1;
-    z[i]=j;
-    if(j==0){
-      i+=1;
-      continue;
+namespace Lib{
+  template<class T>
+  vector<int> z_algorithm(const vector<T>& A){
+    int n=A.size();
+    vector<int> z(n);
+    int i=1,j=0;
+    while(i<n){
+      while(i+j<n&&A[j]==A[i+j])j+=1;
+      z[i]=j;
+      if(j==0){
+        i+=1;
+        continue;
+      }
+      int k=1;
+      while(i+k<n&&k+z[k]<j)z[i+k]=z[k],k+=1;
+      i+=k,j-=k;
     }
-    int k=1;
-    while(i+k<n&&k+z[k]<j)z[i+k]=z[k],k+=1;
-    i+=k,j-=k;
+    z[0]=n;
+    return z;
   }
-  z[0]=n;
-  return z;
 }
