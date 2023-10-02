63e0039d2222b734e8e56449bb1c157fbb86c40d
Nzt3 <nzt3pro@gmail.com>
2023-10-02 22:53:37 +0900

    add Binariy Indexed Tree

---
+++ data_structure/BIT.hpp
@@ -0,0 +1,30 @@
+#include"../template/template.hpp"
+
+/*
+Binary Indexed Tree
+半群 の 区間積 が乗るが、ここでは sum のみを想定する
+1-indexed
+add(p,v) 位置pにvを加算する
+sum(l,r) 区間[l,r)の和を得る
+*/
+
+namespace Lib{
+  struct BIT{
+    vector<ll> a;
+    int sz;
+    BIT (int n){
+      sz=n;
+      a.resize(sz);
+      fill(a.begin(),a.end(),0);
+    }
+    void add(int p,int v){
+      for(;p<sz;p+=p&-p)a[p]+=v;
+    }
+    ll sum(int l,int r){
+      ll ret=0;
+      for(--r;r>0;r-=r&-r)ret+=a[r];
+      for(--l;l>0;l-=l&-l)ret-=a[r];
+      return ret;
+    }
+  };
+}
