88b4c256741f49c97d4e39dd205dd71f5e6c15cc
Nzt3 <nzt3pro@gmail.com>
2023-11-08 23:50:29 +0900

    add modinv

--- math/extGCD.hpp
+++ math/extGCD.hpp
@@ -1,13 +1,20 @@
-#include"../template/template.hpp"
+#include "../template/template.hpp"
 
-namespace Lib{
-  ll extGCD(ll a, ll b, ll &x, ll &y){
-    if(b==0){
-      x=1,y=0;
-      return a;
-    }
-    ll d=extGCD(b,a%b,y,x);
-    y=y-a/b*x;
-    return d;
+namespace Lib {
+ll extGCD(ll a, ll b, ll &x, ll &y) {
+  if (b == 0) {
+    x = 1, y = 0;
+    return a;
   }
+  ll d = extGCD(b, a % b, y, x);
+  y = y - a / b * x;
+  return d;
 }
+ll modinv(ll a, ll m) {
+  ll x = 0, y = 0, d = extGCD(a, m, x, y);
+  if (d != 1) {
+    return -1;
+  }
+  return x;
+}
+}  // namespace Lib
