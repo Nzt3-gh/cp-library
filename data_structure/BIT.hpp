5bf795c46cb6469791de5a01c0460b67ee17d0fc
Nzt3 <nzt3pro@gmail.com>
2023-11-23 15:49:25 +0900

    fix format

--- data_structure/BIT.hpp
+++ data_structure/BIT.hpp
@@ -1,4 +1,4 @@
-#include"../template/template.hpp"
+#include "../template/template.hpp"
 
 /*
 Binary Indexed Tree
@@ -8,20 +8,19 @@ add(p,v) 位置pにvを加算する
 sum(l,r) 区間[l,r)の和を得る
 */
 
-namespace Lib{
-  struct BIT{
-    vector<ll> a;
-    int sz;
-    BIT (int n):
-      sz(n),a(sz+1) {}
-    void add(int p,int v){
-      for(;p<=sz;p+=p&-p)a[p]+=v;
-    }
-    ll sum(int l,int r){
-      ll ret=0;
-      for(--r;r>0;r-=r&-r)ret+=a[r];
-      for(--l;l>0;l-=l&-l)ret-=a[l];
-      return ret;
-    }
-  };
-}
+namespace Lib {
+struct BIT {
+  vector<ll> a;
+  int sz;
+  BIT(int n) : sz(n), a(sz + 1) {}
+  void add(int p, int v) {
+    for (; p <= sz; p += p & -p) a[p] += v;
+  }
+  ll sum(int l, int r) {
+    ll ret = 0;
+    for (--r; r > 0; r -= r & -r) ret += a[r];
+    for (--l; l > 0; l -= l & -l) ret -= a[l];
+    return ret;
+  }
+};
+}  // namespace Lib
