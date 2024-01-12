aaaa517f0be6977d1ecf061242fc2ba7a6d03295
Nzt3 <nzt3pro@gmail.com>
2024-01-12 15:56:54 +0900

    to 0-indexed

--- data_structure/BIT.hpp
+++ data_structure/BIT.hpp
@@ -3,7 +3,7 @@
 /*
 Binary Indexed Tree
 半群 の 区間積 が乗るが、ここでは sum のみを想定する
-1-indexed
+0-indexed
 add(p,v) 位置pにvを加算する
 sum(l,r) 区間[l,r)の和を得る
 */
@@ -12,14 +12,14 @@ namespace Lib {
 struct BIT {
   vector<ll> a;
   int sz;
-  BIT(int n) : sz(n), a(sz + 1) {}
+  BIT(int n) : sz(n), a(n + 1) {}
   void add(int p, int v) {
-    for (; p <= sz; p += p & -p) a[p] += v;
+    for (p += 1; p <= sz; p += p & -p) a[p] += v;
   }
   ll sum(int l, int r) {
     ll ret = 0;
-    for (--r; r > 0; r -= r & -r) ret += a[r];
-    for (--l; l > 0; l -= l & -l) ret -= a[l];
+    for (; r > 0; r -= r & -r) ret += a[r];
+    for (; l > 0; l -= l & -l) ret -= a[l];
     return ret;
   }
 };
