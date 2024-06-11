bf607bf880f4b9a2d1334dc7c6d44e2325e37386
Nzt3 <nzt3pro@gmail.com>
2024-06-12 01:16:17 +0900

    add simple queue

--- data_structure/foldable_queue.hpp
+++ data_structure/foldable_queue.hpp
@@ -85,10 +85,10 @@ struct foldable_queue {
   }
   S front() {
     assert(f_st_P.size() + b_st_P.size() >= 1);
-    if (f_st_P.empty()) {
+    if (f_st_V.empty()) {
       move_bf();
     }
-    return f_st_P.back();
+    return f_st_V.back();
   }
   S prod() {
     assert(f_st_P.size() + b_st_P.size() >= 1);
@@ -101,4 +101,41 @@ struct foldable_queue {
   size_t size() { return f_st_P.size() + b_st_P.size(); }
 };
 
+template <class S, auto op, auto e>
+struct simple_foldable_queue {
+  static_assert(is_convertible_v<decltype(op), function<S(S, S)>>);
+  vector<S> f_P, b_V;
+  S b_P;
+  simple_foldable_queue() : f_P(0), b_V(0) { b_P = e(); }
+  void push(S v) {
+    b_V.push_back(v);
+    b_P = op(b_P, v);
+  }
+  void move_bf() {
+    assert(f_P.empty());
+    while (!b_V.empty()) {
+      f_P.push_back(b_V.back());
+      b_V.pop_back();
+    }
+    for (int i = 1; i < (int)f_P.size(); i++) {
+      f_P[i] = op(f_P[i], f_P[i - 1]);
+    }
+    b_P = e();
+  }
+  void pop() {
+    assert(f_P.size() + b_V.size() >= 1);
+    if (f_P.empty()) {
+      move_bf();
+    }
+    f_P.pop_back();
+  }
+  S prod() {
+    assert(f_P.size() + b_V.size() >= 1);
+    if (f_P.empty()) {
+      return b_P;
+    }
+    return op(f_P.back(), b_P);
+  }
+  size_t size() { return f_P.size() + b_V.size(); }
+};
 }  // namespace Lib
