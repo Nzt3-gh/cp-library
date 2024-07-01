e1892a108ddb432b84eb37dff2ca5fe9ca767747
Nzt3 <nzt3pro@gmail.com>
2024-07-01 12:09:56 +0900

    add compress

---
+++ misc/compress.hpp
@@ -0,0 +1,35 @@
+/*
+座標圧縮
+
+Compress (vector<T> V) :  O(|V|)
+vector Vの座標圧縮を構築
+
+idx(v): O(log(|V|))
+値vの座標圧縮後のindexを取得
+
+val(i): O(1)
+i番目の値を取得
+*/
+
+namespace Lib{
+  template<class T>
+  struct Compress{
+    vector<T> v;
+    long long sz;
+    Compress(vector<T> v_): v(v_){
+      sort(v.begin(),v.end());
+      v.erase(unique(v.begin(),v.end()),v.end());
+      sz=v.size();
+    }
+    int idx(T x){
+      int ret=lower_bound(v.begin(),v.end(),x)-v.begin();
+      assert(ret<sz);
+      assert(v[ret]==x);
+      return ret;
+    }
+    T val(int i){
+      assert(i>=0&&i<sz);
+      return v[i];
+    }
+  };
+} // namespace Lib
