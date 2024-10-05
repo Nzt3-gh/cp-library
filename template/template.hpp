1a115aad65814ab9c15b0c98beb941e7154d3407
Nzt3 <nzt3pro@gmail.com>
2024-10-06 01:26:18 +0900

    add macro

--- template/template.hpp
+++ template/template.hpp
@@ -5,3 +5,6 @@ using namespace std;
 using ll = long long;
 using ull= unsigned long long;
 constexpr int MOD=998244353;
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define rep2(i,l,r) for(int i=(l);i<(int)(r);i++)
+#define all(v) v.begin(),v.end()
